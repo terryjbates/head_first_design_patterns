ed5aeea37cea5fd73def1bee9ed1933d292c12ed
Terry Bates <terryjbates@gmail.com>
2016-09-21 23:47:36 -0700

    Remove execute() method in lieu of callable, create GarageDoor code.

--- ch5_the_command_pattern/command_pattern.py
+++ ch5_the_command_pattern/command_pattern.py
@@ -11,18 +11,19 @@ class Command(object):
     def __call__(self):
         self.execute()
 
-    @abc.abstractmethod
-    def execute(self):
-        return NotImplementedError
-
 
 class LightOnCommand(Command):
-    def __init__(self, light):
-        self.light = light
+    def __init__(self, light_obj):
+        assert isinstance(light_obj, Light)
+        self.light = light_obj
         super(LightOnCommand, self).__init__(self.light.on)
 
-    def execute(self):
-        self.light.on()
+
+class GarageDoorOpenCommand(Command):
+    def __init__(self, garage_door_obj):
+        assert isinstance(garage_door_obj, GarageDoor)
+        self.garage_door = garage_door_obj
+        super(GarageDoorOpenCommand, self).__init__(self.garage_door.up)
 
 
 class Light(object):
@@ -34,6 +35,26 @@ class Light(object):
 
     def off(self):
         print("Light Off")
+
+
+class GarageDoor(object):
+    def __init__(self):
+        pass
+
+    def up(self):
+        print("Garage door goes up.")
+
+    def down(self):
+        print("Garage door goes down.")
+
+    def stop(self):
+        print("Garage door stops moving.")
+
+    def light_on(self):
+        print("Garage light goes on.")
+
+    def light_off(self):
+        print("Garage light goes off.")
 
 
 class SimpleRemoteControl(object):
@@ -52,7 +73,10 @@ if __name__ == '__main__':
     remote = SimpleRemoteControl()
     light = Light()
     light_on = LightOnCommand(light)
-
+    garage_door = GarageDoor()
+    garage_open = GarageDoorOpenCommand(garage_door)
 
     remote.set_command(light_on)
+    remote.button_was_pressed()
+    remote.set_command(garage_open)
     remote.button_was_pressed()
